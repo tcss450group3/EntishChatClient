d8de98fb7360316c1d5b10399b5a19b24fd0f90c
ruitaoyu <ruitaoyu@uw.edu>
2019-02-23 17:06:43 -0800

    UI stuff

--- app/src/main/java/com/example/blw13/chatclient/HomeActivity.java
+++ app/src/main/java/com/example/blw13/chatclient/HomeActivity.java
@@ -11,6 +11,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -44,6 +45,10 @@ public class HomeActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+//
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        toolbar.set
 
         Intent intent = getIntent();
         Bundle args = new Bundle();
