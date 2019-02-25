57495860e9a4367b6739fb2915c13b3afd03d0c0
BuildTools <unconfigured@null.spigotmc.org>
2019-02-24 22:03:43 -0800

    connection mostly done, just refining left

--- app/src/main/java/com/example/blw13/chatclient/HomeActivity.java
+++ app/src/main/java/com/example/blw13/chatclient/HomeActivity.java
@@ -16,7 +16,6 @@ import android.widget.TextView;
 
 import com.example.blw13.chatclient.Content.Connection;
 import com.example.blw13.chatclient.Model.Credentials;
-import com.example.blw13.chatclient.dummy.ConversationListContent;
 import com.example.blw13.chatclient.utils.GetAsyncTask;
 import com.example.blw13.chatclient.utils.SendPostAsyncTask;
 
@@ -30,7 +29,7 @@ import java.util.List;
 public class HomeActivity extends AppCompatActivity implements
         ConnectionListFragment.OnListFragmentInteractionListener,
         WaitFragment.OnWaitFragmentInteractionListener,
-        ChatListFragment.OnChatListFragmentInteractionListener,
+        ConversationListFragment.OnChatListFragmentInteractionListener,
         OneConnectionFragment.OnProfileFragmentInteractionListener,
         NewConnection.OnNewConnectionFragmentInteractionListener{
 
