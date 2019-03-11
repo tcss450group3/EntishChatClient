15934189b2339daf3a87f57f6cf798f1dbc298f9
ruitaoyu <ruitaoyu@uw.edu>
2019-03-11 14:27:51 -0700

    Update HomeActivity.java

--- app/src/main/java/com/example/blw13/chatclient/HomeActivity.java
+++ app/src/main/java/com/example/blw13/chatclient/HomeActivity.java
@@ -783,8 +783,6 @@ public class HomeActivity extends AppCompatActivity implements
         args.putSerializable(getString(R.string.keys_intent_jwt), mJwToken);
         args.putSerializable(getString(R.string.keys_intent_credentials), mCredentials);
 
-        onWaitFragmentInteractionHide();
-
         FavoriteLocationsFragment flFrag = new FavoriteLocationsFragment();
 
         flFrag.setArguments(args);
