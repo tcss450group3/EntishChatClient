a850348b9f1aacc19a7444eccf6c927b1561896e
Rob <wolfr1@uw.edu>
2019-03-15 17:36:31 -0700

    Doc

--- app/src/main/java/com/example/blw13/chatclient/GetIconAsyncTask.java
+++ app/src/main/java/com/example/blw13/chatclient/GetIconAsyncTask.java
@@ -3,5 +3,9 @@ package com.example.blw13.chatclient;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 
+/**
+ * Abstract class to get an Bitmap icon asynchronously
+ * @author Robert Wolf
+ */
 abstract class GetIconAsyncTask extends AsyncTask<Void, String, Bitmap> {
 }
